b7e5bbdb5c05b802f3480bc228e8c528d39d106a
nipunraj <nipunraj99@gmail.com>
2023-04-04 15:03:34 +0530

    added cookieyes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import Script from "next/script";
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    (<Component {...pageProps} />),
+    (
+      <script
+        id="cookieyes"
+        type="text/javascript"
+        src="https://cdn-cookieyes.com/client_data/440dc8f4202424ea7c3394f4/script.js"
+      ></script>
+    )
+  );
 }
