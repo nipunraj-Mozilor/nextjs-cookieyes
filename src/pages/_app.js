59446690c676a9f4cb1e15b374a239e2a4bb8c22
nipunraj <nipunraj99@gmail.com>
2023-04-04 15:06:43 +0530

    added strategy for cookiyes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,7 @@ export default function App({ Component, pageProps }) {
         id="cookieyes"
         type="text/javascript"
         src="https://cdn-cookieyes.com/client_data/440dc8f4202424ea7c3394f4/script.js"
+        strategy="beforeInteractive"
       ></script>
     )
   );
